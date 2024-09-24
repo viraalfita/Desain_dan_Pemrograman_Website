ebba165dea7146878c9894feda3b82f3c58f9188
viraalfita <viraalfita1813@gmail.com>
2024-09-24 15:44:39 +0700

    percobaan 1

---
+++ Pertemuan5/fungsi.php
@@ -0,0 +1,11 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum <br>";
+    echo "Perkenalkan nama saya Vira Alfita <br>";
+    echo "Senang berkenalan dengan anda <br>";
+}
+
+perkenalan();
+perkenalan();
+?>
