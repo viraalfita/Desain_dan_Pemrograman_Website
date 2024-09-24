ad56df6ae1953643dd47e00720276ff854a20b35
viraalfita <viraalfita1813@gmail.com>
2024-09-24 16:10:33 +0700

    parameter dengan nilai default

--- Pertemuan5/fungsi.php
+++ Pertemuan5/fungsi.php
@@ -1,6 +1,6 @@
 <?php
 
-function perkenalan($nama, $salam){
+function perkenalan($nama, $salam = "Assalamualaikum") {
     echo $salam . ", ";
     echo "Perkenalkan nama saya ". $nama . "<br>";
     echo "Senang berkenalan dengan anda <br>";
@@ -13,5 +13,5 @@ echo "<hr>";
 $saya = "Vira";
 $ucapanSalam = "Selamat pagi";
 
-perkenalan($saya, $ucapanSalam);
+perkenalan($saya);
 ?>
