5b9207be921293f93c2c6874c04626cb9d56ecac
viraalfita <viraalfita1813@gmail.com>
2024-09-25 17:51:43 +0700

    percobaan 1

---
+++ Pertemuan5/jquery/praktik_jquery.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <title>Praktikum JQuery</title>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script>
+            $("#buttonl").click(function(){
+                alert("Ini praktikum minggu ke-5");
+            })
+    </script>
+</head>
+<body>
+    <input type="button" id="buttonl" value="Klik Saya!">
+</body>
+</html>
