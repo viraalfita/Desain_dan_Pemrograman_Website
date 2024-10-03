f1bb50e377de2ffa91f10e35e812f03e7fc96c45
viraalfita <viraalfita1813@gmail.com>
2024-10-03 14:37:48 +0700

    percobaan indexed array

---
+++ Pertemuan6/indexed_array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <title>Indexed Array</title>
+</head>
+<body>
+    <h2>Array Terindeks</h2>
+
+    <?php
+    $listDosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+    
+    for ($i=0; $i < count($listDosen); $i++) { 
+        echo $listDosen[$i] . "<br>";
+    }
+    ?>
+</body>
+</html>
