51ff7bcd16420a6f62460e9ef43905715b687ad6
viraalfita <viraalfita1813@gmail.com>
2024-10-04 09:36:20 +0700

    percobaan date

---
+++ Pertemuan6/date.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Date</title>
+    </head>
+    <body>
+        <h3>Date</h3>
+        <?php
+            echo "Today is " . date("Y/m/d") . "<br>";
+            echo "Today is " . date("Y.m.d") . "<br>";
+            echo "Today is " . date("Y-m-d") . "<br>";
+            echo "Today is " . date("l");
+        ?>
+    </body>
+</html>
