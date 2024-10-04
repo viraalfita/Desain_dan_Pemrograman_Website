9d6556fca95bd40a1cfe68ae8c4ca07e00829936
viraalfita <viraalfita1813@gmail.com>
2024-10-04 09:44:10 +0700

    percobaan time

---
+++ Pertemuan6/time.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Time</title>
+    </head>
+    <body>
+        <h3>Time</h3>
+        <?php
+            date_default_timezone_set('Asia/Jakarta');
+            echo date("h:i:sa");
+        ?>
+    </body>
+</html>
