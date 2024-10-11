c6a727bd2b6112db9ebf59b26f227ea6a2c87ba1
viraalfita <viraalfita1813@gmail.com>
2024-10-11 19:35:30 +0700

    percobaan regex

---
+++ Pertemuan7/regex.php
@@ -0,0 +1,12 @@
+<?php
+
+$pattern = '/o{1,3}/'; 
+$text = 'god is good.';
+
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok";
+}
+
+?>
