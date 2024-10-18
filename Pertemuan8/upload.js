3d13b3a6867bc25384d3d2e63a6b1b28365ee560
viraalfita <viraalfita1813@gmail.com>
2024-10-18 12:08:32 +0700

    percobaan 4

--- Pertemuan8/upload.js
+++ Pertemuan8/upload.js
@@ -1,9 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    $('#file').change(function () {
+        if (this.files.length > 0) {
+            $("#upload-button").prop('disabled', false).css('opacity', 1);
+        } else {
+            $("#upload-button").prop('disabled', true).css('opacity', 0.5);
+        }
+    })
     $('#upload-form').submit(function(e){
         e.preventDefault();
 
         var formData = new FormData(this);
-        
+
         $.ajax({
             type: 'POST',
             url: 'upload_ajax.php',
